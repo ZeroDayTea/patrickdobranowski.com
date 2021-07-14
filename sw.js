488325f7d9398c50c506c1495ad3f1247d44309a
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-14 08:26:17 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-00f2ac8c82ab9c0ea477.js"
+    "url": "webpack-runtime-c96f8a9056d764a49a59.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b86ab3a3130cb041a469e234774a4043"
+    "revision": "8c0a1369b0a25a81d1465553860c256d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
